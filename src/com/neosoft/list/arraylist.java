dcac5b94a40b7c99e07d25afd1bfdd7d297f5c14
rschavan1709 <rschavan1709@gmail.com>
2023-01-05 18:45:07 +0530

    2nd commit

---
+++ src/com/neosoft/list/arraylist.java
@@ -0,0 +1,14 @@
+package com.neosoft.list;
+
+import java.util.ArrayList;
+
+public class arraylist {
+public static void main(String[] args) {
+	ArrayList< Integer> a=new ArrayList<>();
+	a.add(21);
+	a.add(23);
+	a.add(56);
+	a.add(78);
+	System.out.println(a);
+}
+}
